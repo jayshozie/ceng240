4202042862b76e54b1113eccf9f45a0273bdf909
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-05-26 10:09:42 +0300

    adding a new lab question, don't have the entirety of the question yet

---
+++ lab-questions/W6buy5pay4.py
@@ -0,0 +1,18 @@
+"""
+This is from another lab in week 6.
+
+Basically, write a class that allows you to add items and prices, which you
+don't pay the cheapest of 5.
+
+"""
+
+
+class Buy5Pay4:
+    def __init__(self):
+        pass
+
+    def addItems(self, items, prices):
+        pass
+
+    def total(self):
+        pass
